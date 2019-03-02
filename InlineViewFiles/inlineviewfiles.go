99328cbb419ad1c717aa790433c5ed58b36c200a
Andy Roberts <avrpvrt@gmail.com>
2019-03-02 14:31:11 +0000

    file i/o

---
+++ InlineViewFiles/inlineviewfiles.go
@@ -0,0 +1,33 @@
+package inlineviewfiles
+
+import (
+	"fmt"
+	"os"
+)
+
+// OpenReadOnlyFile is a public function to request a file to be opened returning the file struct
+func OpenReadOnlyFile(name string) *os.File {
+	var err error
+
+	handle, err := os.OpenFile(name, os.O_RDONLY, 0666)
+	if err != nil {
+		fmt.Println("Failed to open file", name, "due to", err)
+	} else {
+		fmt.Println("Opened file", name)
+	}
+	return handle
+}
+
+// CloseFile is a public function to close an existing opened file.
+func CloseFile(handle *os.File) {
+	handle.Close()
+	fmt.Println("Closed file", handle.Name())
+}
+
+// DoesFileExist returns true if the file is found on at the named location
+func DoesFileExist(name string) bool {
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
