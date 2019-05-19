9177d992927d1ea18d14d648cf6b42fbb3b430d1
Andy Roberts <avrpvrt@gmail.com>
2019-05-19 12:03:31 +0100

    add utilities benchmark tests

--- utilities/common_test.go
+++ utilities/common_test.go
@@ -1,6 +1,10 @@
 package utilities
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestLogFileNameReturnsSameFileName(t *testing.T) {
 	expectedName := LogFileName()
@@ -10,6 +14,15 @@ func TestLogFileNameReturnsSameFileName(t *testing.T) {
 		t.Error("Failed to create the same log file name within the same day:", expectedName, "vs", res)
 	}
 }
+
+func BenchmarkLogFileName(b *testing.B) {
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = LogFileName()
+	}
+}
+
 func TestSizeAsStringShowsBytes(t *testing.T) {
 	res := SizeAsString(124)
 	expectedString := "124 bytes"
@@ -18,6 +31,7 @@ func TestSizeAsStringShowsBytes(t *testing.T) {
 		t.Error("Failed to show size as string in Bytes:", res)
 	}
 }
+
 func TestSizeAsStringShowsKilobytes(t *testing.T) {
 	res := SizeAsString(1024)
 	expectedString := "1 Kbytes"
@@ -26,6 +40,7 @@ func TestSizeAsStringShowsKilobytes(t *testing.T) {
 		t.Error("Failed to show size as string in Kilobytes:", res)
 	}
 }
+
 func TestSizeAsStringShowsMegabytes(t *testing.T) {
 	res := SizeAsString(1024 * 1024)
 	expectedString := "1 Mbytes"
@@ -34,6 +49,19 @@ func TestSizeAsStringShowsMegabytes(t *testing.T) {
 		t.Error("Failed to show size as string in Megabytes:", res)
 	}
 }
+
+func BenchmarkSizeAsString(b *testing.B) {
+	// seed with varying number at each execution of function
+	rand.Seed(time.Now().UnixNano())
+	// select random number between 1Kb and 20Mb
+	sampleSize := 1024 + rand.Intn(24117248-1024)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = SizeAsString(sampleSize)
+	}
+}
+
 func TestSizeAsStringShowsGigabytes(t *testing.T) {
 	res := SizeAsString(1024 * 1024 * 1024)
 	expectedString := "1 Gbytes"
@@ -42,6 +70,7 @@ func TestSizeAsStringShowsGigabytes(t *testing.T) {
 		t.Error("Failed to show size as string in Gigabytes", res)
 	}
 }
+
 func TestHashDataStringCreatesSameTestHash(t *testing.T) {
 	res := HashDataString("TestHashString")
 	expectedHash := "RX9f5OrTt98VhErEp6NhJ9oz7VQ="
@@ -49,4 +78,17 @@ func TestHashDataStringCreatesSameTestHash(t *testing.T) {
 	if res != expectedHash {
 		t.Error("Failed to produce expected hash string:", res)
 	}
+}
+
+func BenchmarkHashDataString(b *testing.B) {
+	input := "TestHashString"
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = HashDataString(input)
+	}
+}
+
+func TestLogMemStats(t *testing.T) {
+	t.Skip("LogMemStats not yet known to have a valid test case")
 }
