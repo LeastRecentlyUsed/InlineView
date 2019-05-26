413aba9022d7aa000c7a9ab40d498eaff849b063
Andy Roberts <avrpvrt@gmail.com>
2019-05-26 16:53:44 +0100

    more datastore tests

--- model/datastore.go
+++ model/datastore.go
@@ -2,11 +2,13 @@ package model
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"io"
 	"strings"
 )
 
-// readIndex loads the associated index for a price store.
+// readIndex converts a read-stream for an index of a price store to a searchable map
 func readIndex(f io.ReadCloser) (map[string]string, int) {
 	s := bufio.NewScanner(f)
 	priceKeys := map[string]string{}
@@ -19,4 +21,23 @@ func readIndex(f io.ReadCloser) (map[string]string, int) {
 	}
 	f.Close()
 	return priceKeys, keysSize
+}
+
+//writeIndex converts the map of an index for a price store to a write-stream.
+func writeIndex(priceKeys map[string]string) io.Writer {
+	b := new(bytes.Buffer)
+	w := io.Writer(b)
+
+	for key, val := range priceKeys {
+		fmt.Fprintf(w, "%s|%s\n", key, val)
+	}
+	return w
+}
+
+// foundIndex searches for the price key and returns the boolean result
+func foundIndex(priceKeys map[string]string, key string) bool {
+	if _, found := priceKeys[key]; found {
+		return true
+	}
+	return false
 }
--- model/datastore_test.go
+++ model/datastore_test.go
@@ -4,8 +4,24 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"testing"
 )
+
+var priceKeys map[string]string
+
+func TestMain(m *testing.M) {
+	setup()
+	res := m.Run()
+	os.Exit(res)
+}
+
+func setup() {
+	priceKeys = map[string]string{
+		"xxx": "MK17 9AU",
+		"yyy": "MK17 8AQ",
+	}
+}
 
 func TestReadIndexReturnsValidMapAndNumberOfEntries(t *testing.T) {
 	r := ioutil.NopCloser(bytes.NewReader([]byte("xxx|MK17 9AU\nyyy|MK17 8AQ")))
@@ -19,19 +35,43 @@ func TestReadIndexReturnsValidMapAndNumberOfEntries(t *testing.T) {
 	}
 }
 
-func TestWriteIndexCreatesValidWriteBuffer(t *testing.T) {
+func TestReadIndexCanReturnEmptyMap(t *testing.T) {
+	r := ioutil.NopCloser(bytes.NewReader([]byte("")))
+
+	m, c := readIndex(r)
+	if c > 0 {
+		t.Error("Invalid number of map entries created from empty reader.  Expected zero but got:", c)
+	}
+	if len(m) > 0 {
+		t.Error("Map should be empty but has a length greater than zero:", len(m))
+	}
+}
+
+func TestWriteIndexCreatesValidWriterContents(t *testing.T) {
 	expectedBuff := "xxx|MK17 9AU\nyyy|MK17 8AQ\n"
-	m := map[string]string{
-		"xxx": "MK17 9AU",
-		"yyy": "MK17 8AQ",
+	res := writeIndex(priceKeys)
+	out := fmt.Sprint(res)
+
+	if out != expectedBuff {
+		t.Error("Invalid write buffer created from map", out)
+	}
+}
+
+func TestFoundIndexReturnsValidBool(t *testing.T) {
+	var indexTable = []struct {
+		in1 map[string]string
+		in2 string
+		out bool
+	}{
+		{priceKeys, "xxx", true},
+		{priceKeys, "yyy", true},
+		{priceKeys, "zzz", false},
 	}
 
-	b := new(bytes.Buffer)
-	for key, val := range m {
-		fmt.Fprintf(b, "%s|%s\n", key, val)
-	}
-
-	if b.String() != expectedBuff {
-		t.Error("Invalid write buffer created from map", b.String())
+	for _, testEntry := range indexTable {
+		res := foundIndex(testEntry.in1, testEntry.in2)
+		if res != testEntry.out {
+			t.Error("Invalid response from index search.  Expecting", testEntry.out, "but received", res, "for key:", testEntry.in2)
+		}
 	}
 }
